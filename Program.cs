107214a1a38d2588bf0b91c44c1d85604ef03b07
rhommardev2023 <rjmlp02@gmail.com>
2025-06-13 01:52:41 -0700

    creando una carpeta de clases

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//primeiros passos com C#
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("teste");
 Console.WriteLine("nombre");
+
