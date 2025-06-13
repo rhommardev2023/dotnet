af0d67e752d4733e1279c7a49cfb0e0bab68f8df
rhommardev2023 <rjmlp02@gmail.com>
2025-06-13 02:49:49 -0700

    .

--- classes/Pessoa.cs
+++ classes/Pessoa.cs
@@ -8,11 +8,11 @@ namespace dotnet.classes // namespace para organizar o codigo
     public class Pessoa // classe Pessoa que representa uma pessoa
     {
         //atributos se inica con prop 
-        public string Nome { get; set; } 
+        public string Nome { get; set; }
         public int Idade { get; set; } //get y set son los metodos de acceso get= obtener y set= asignar
 
-// creacion de um metodo que es la accion a ejecutar
-        public void Apresentar() // metodo que imprime na tela
+        // creacion de um metodo que es la accion a ejecutar
+        public void Apresentar() // metodo que imprime na tela ou chamada do metodo
         {
             Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
         }
