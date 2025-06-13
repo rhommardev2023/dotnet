d2ebf0558239c60dfce15ff06f36ce6bfd2859ba
rhommardev2023 <rjmlp02@gmail.com>
2025-06-13 02:16:42 -0700

    fue creada la clase

--- classes/Pessoa.cs
+++ classes/Pessoa.cs
@@ -7,6 +7,14 @@ namespace dotnet.classes
 {
     public class Pessoa
     {
-        
+        //atributos se inica con prop 
+        public string Nome { get; set; } 
+        public int Idade { get; set; } //get y set son los metodos de acceso get= obtener y set= asignar
+
+// creacion de um metodo que es la accion a ejecutar
+        public void Apresentar() // metodo que imprime na tela
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
+        }
     }
 }
