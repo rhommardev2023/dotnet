e409baee1274a785eb142939f88e6a4df4638852
rhommardev2023 <rjmlp02@gmail.com>
2025-06-13 02:28:50 -0700

    instancia en el archivo program

--- Program.cs
+++ Program.cs
@@ -2,7 +2,17 @@
 
 //primeiros passos com C#
 
-Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Hello, World!");
 Console.WriteLine("teste");
-Console.WriteLine("nombre");
+Console.WriteLine("nombre");*/
+
+using dotnet.classes; // importando o namespace dotnet.classes 
+// instanciando um objeto da classe Pessoa
+
+Pessoa pessoa1 = new Pessoa(); //instancia de um objeto da classe Pessoa   
+
+pessoa1.Nome = "João"; // atribuindo valor ao atributo Nome
+pessoa1.Idade = 30; // atribuindo valor ao atributo Idade       
+// chamando o metodo Apresentar
+pessoa1.Apresentar(); // chamando o metodo Apresentar da classe Pessoa  
 
